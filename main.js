4194f371aa503fd8dc8501be4eaaa5b4bc19c975
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2021-11-09 15:27:34 +0530

    Added Javascript Form Validations

---
+++ main.js
@@ -0,0 +1,69 @@
+const form = document.querySelector("form");
+const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+})
+
+const checkInputs = () => {
+  let firstNameValue = firstName.value.trim();
+  let lastNameValue = lastName.value.trim();
+  let emailValue = email.value.trim();
+  let passwordValue = password.value.trim();
+  if(firstNameValue === "") {
+    showError(firstName, `First Name cannot be blank`);
+  } else {
+    removeError(firstName);
+  }
+  if(lastNameValue === "") {
+    showError(lastName, `Last Name cannot be blank`);
+  } else {
+    removeError(lastName);
+  }
+  if(emailValue === "") {
+    showError(email, `Email cannot be blank`)
+  } else if(!emailValid(emailValue)) {
+    showError(email, `Invalid email`);
+  } else {
+    removeError(email);
+  }
+  if(passwordValue === "") {
+    showError(password, `Password cannot be blank`)
+  } else {
+    removeError(password);
+  }
+  if (
+    // Alerts a success message if the form is successfully submitted
+    firstNameValue !== "" &&
+    lastNameValue !== "" &&
+    emailValue !== "" &&
+    emailValid(emailValue) &&
+    passwordValue !== ""
+  ) {
+    alert(`Form Successfully Submitted`);
+  }
+}
+
+const showError = (input, message) => {
+  const formGroup = input.parentElement;
+  const small = formGroup.querySelector("small");
+  formGroup.classList.remove("success");
+  formGroup.classList.add("error");
+  small.innerText = message;
+}
+
+const removeError = (input) => {
+  const formGroup = input.parentElement;
+  formGroup.classList.remove("error");
+  formGroup.classList.add("success");
+}
+
+const emailValid = (email) => {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
